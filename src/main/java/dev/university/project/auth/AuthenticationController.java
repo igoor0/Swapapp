dedbba91fb0eb709b0b9fe14586b11a3e766c224
Igoor0 <igor.kohsin@gmail.com>
2024-05-15 14:30:44 +0200

    Add Authorization / Authentication logic including auth - Service, Controller, Request, Response
    Remove old auth logic

---
+++ src/main/java/dev/university/project/auth/AuthenticationController.java
@@ -0,0 +1,26 @@
+package dev.university.project.auth;
+
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest loginRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(loginRequest));
+    }
+}
--- src/main/java/dev/university/project/auth/ROLES.java
+++
@@ -1,5 +0,0 @@
-package dev.university.project.auth;
-
-public enum ROLES {
-    ADMIN, USER, GUEST
-}
