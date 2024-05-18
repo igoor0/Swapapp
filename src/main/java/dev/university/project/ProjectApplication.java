0a8f866c966348f838e3f03f5e4d78523bdefc3e
Igoor0 <igor.kohsin@gmail.com>
2024-05-18 16:06:43 +0200

    Bugfix lack of @Bean

--- src/main/java/dev/university/project/ProjectApplication.java
+++ src/main/java/dev/university/project/ProjectApplication.java
@@ -2,6 +2,7 @@ package dev.university.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,7 @@ public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
