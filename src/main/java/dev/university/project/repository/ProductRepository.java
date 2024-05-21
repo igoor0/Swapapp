fba0fe9adba0ca4bdfbe56772baa0f9f845aa487
Igoor0 <igor.kohsin@gmail.com>
2024-05-21 21:38:29 +0200

    Change Repositories to avoid compatibility issues

--- src/main/java/dev/university/project/repository/ProductRepository.java
+++ src/main/java/dev/university/project/repository/ProductRepository.java
@@ -5,9 +5,12 @@ import dev.university.project.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String>{
     Product findByName(String name);
     Product findByCategory(Category category);
     Product findByPrice(double price);
+    List<Product> findAllByProductOwner(String productOwnerId);
 }
--- src/main/java/dev/university/project/repository/TransactionRepository.java
+++ src/main/java/dev/university/project/repository/TransactionRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    Optional<Transaction> findById(String id);
+    List<Transaction> findByBuyerId(String buyerId);
+    List<Transaction> findBySellerId(String sellerId);
 }
